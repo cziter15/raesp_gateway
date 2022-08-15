54a690d993c7fa60464d8bb3d49e70da4af1a2da
cziter15 <chris.streh@gmail.com>
2022-08-15 19:24:53 +0200

    Fix initialization

--- firmware/src/apps/raesp/comps/RadioCommander.cpp
+++ firmware/src/apps/raesp/comps/RadioCommander.cpp
@@ -9,7 +9,7 @@ using namespace std::placeholders;
 
 namespace raesp::comps
 {
-	RadioCommander::RadioCommander(uint8_t ssPin, uint8_t dio0pin, uint8_t rstPin, uint8_t dio2pin, ksLedWP wifiLed, ksLedWP radioLed)
+	RadioCommander::RadioCommander(uint8_t ssPin, uint8_t dio0pin, uint8_t rstPin, uint8_t dio2pin, ksLedWP wifiLedWp, ksLedWP radioLedWp)
 		: radioLedWp(radioLedWp), wifiLedWp(wifiLedWp)
 	{
 		/* Instantiate radio PHY/Module. */
